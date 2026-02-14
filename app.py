55cdd59d5141ba77dd2e09fa763eaf9b5b918398
rhemayl <rhema9735@gmail.com>
2026-02-14 12:15:54 +0000

    threading and loading page

--- app.py
+++ app.py
@@ -1,33 +1,61 @@
 from flask import Flask, render_template, request, send_file
 import os
 import time
-import uuid
+import threading
 
 app = Flask(__name__)
+video_path = "static/video/NewThreeBodyProblem.mp4"
+flag_path = "static/video/done.flag"
+
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
+
+def generate_video(position, velocity, mass1, mass2, mass3, video_path):
+    from three_body_simulation import threebp 
+    threebp(position, velocity, mass1, mass2, mass3, output_path=video_path)
+    with open(flag_path, "w") as f:
+        f.write("done")
+
+
+
+
 @app.route("/run-simulation", methods=["POST"])
-
-
 def run_simulation():
     position = request.form.get("position")
     velocity = request.form.get("velocity")
     mass1 = request.form.get("mass1")
     mass2 = request.form.get("mass2")
     mass3 = request.form.get("mass3")
-    from three_body_simulation import threebp 
-    video_path = "static/video/NewThreeBodyProblem.mp4"
-    message = threebp(position, velocity, mass1, mass2, mass3, output_path=video_path)
-    print(f"DEBUG: Video exists? {os.path.exists(video_path)}")
+
+
+    if os.path.exists(video_path):
+        os.remove(video_path)
+
+    if os.path.exists(flag_path):
+        os.remove(flag_path)
+    
+    thread = threading.Thread(
+        target=generate_video,
+        args=(position, velocity, mass1, mass2, mass3, video_path)
+    )
+    thread.start()
+    
+    return render_template("loading.html")
+
+@app.route("/check_video")
+def check_video():
+    return {"ready": os.path.exists(flag_path)}
+
+@app.route("/result")
+def result():
     return render_template(
         "index.html",
-        message=message,
         video_url=video_path,
-        cache_bust=time.time()
-    )
+        cache_bust=time.time())
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, use_reloader=False)
